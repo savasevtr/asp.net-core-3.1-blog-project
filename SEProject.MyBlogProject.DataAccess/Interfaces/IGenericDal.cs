e863076e9fa461d14e3f2e81f90df9b6bfb7b35d
savasevtr <savas.ev@yahoo.com>
2020-12-07 21:13:34 +0300

    DataAccess - IGenericDal

---
+++ SEProject.MyBlogProject.DataAccess/Interfaces/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using SEProject.MyBlogProject.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SEProject.MyBlogProject.DataAccess.Interfaces
+{
+    public interface IGenericDal<TEntity> where TEntity : class, ITable, new()
+    {
+        Task<List<TEntity>> GetAllAsync();
+        Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task RemoveAsync(TEntity entity);
+    }
+}
