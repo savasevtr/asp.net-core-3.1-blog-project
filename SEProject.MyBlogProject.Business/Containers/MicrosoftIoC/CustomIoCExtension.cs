ec47e6295e1774cef1c38c24c299fc3fab8f13e7
savasevtr <savas.ev@yahoo.com>
2020-12-07 22:41:04 +0300

    updated

---
+++ SEProject.MyBlogProject.Business/Containers/MicrosoftIoC/CustomIoCExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SEProject.MyBlogProject.Business.Concrete;
+using SEProject.MyBlogProject.Business.Interfaces;
+using SEProject.MyBlogProject.DataAccess.Concrete.EntityFrameworkCore.Repositories;
+using SEProject.MyBlogProject.DataAccess.Interfaces;
+
+namespace SEProject.MyBlogProject.Business.Containers.MicrosoftIoC
+{
+    public static class CustomIoCExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericDal<>), typeof(EfGenericRepository<>));
+            services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
+        }
+    }
+}
