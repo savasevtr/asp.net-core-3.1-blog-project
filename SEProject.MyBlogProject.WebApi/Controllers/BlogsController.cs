efd5a486f31df357f2701fb4070b6b64519f058e
savasevtr <savas.ev@yahoo.com>
2020-12-09 21:38:38 +0300

    update

--- SEProject.MyBlogProject.WebApi/Controllers/BlogsController.cs
+++ SEProject.MyBlogProject.WebApi/Controllers/BlogsController.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SEProject.MyBlogProject.Business.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using SEProject.MyBlogProject.Entities.Concrete;
 using System.Threading.Tasks;
 
 namespace SEProject.MyBlogProject.WebApi.Controllers
@@ -29,6 +26,35 @@ namespace SEProject.MyBlogProject.WebApi.Controllers
         public async Task<IActionResult> GetById(int id)
         {
             return Ok(await _blogService.FindById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Blog blog)
+        {
+            await _blogService.AddAsync(blog);
+
+            return Created("", blog);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Blog blog)
+        {
+            if (id != blog.Id)
+            {
+                return BadRequest("geçersiz id");
+            }
+
+            await _blogService.UpdateAsync(blog);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _blogService.RemoveAsync(new Blog { Id = id });
+
+            return NoContent();
         }
     }
 }
