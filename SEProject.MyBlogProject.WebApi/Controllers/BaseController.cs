38b51f730d30abe1e630a850c6261028175b67b9
savasevtr <savas.ev@yahoo.com>
2020-12-10 20:44:47 +0300

    BaseController - Upload File Method

---
+++ SEProject.MyBlogProject.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SEProject.MyBlogProject.WebApi.Enums;
+using SEProject.MyBlogProject.WebApi.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SEProject.MyBlogProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        public async Task<UploadModel> UploadFile(IFormFile file, string contentType)
+        {
+            UploadModel uploadModel = new UploadModel();
+            if (file != null)
+            {
+                if (file.ContentType != contentType)
+                {
+                    uploadModel.ErrorMessage = "Geçersiz dosya türü";
+                    uploadModel.UploadState = UploadState.Error;
+                    return uploadModel;
+                }
+                else
+                {
+                    var newName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img" + newName);
+                    var stream = new FileStream(path, FileMode.Create);
+                    await file.CopyToAsync(stream);
+
+                    uploadModel.NewName = newName;
+                    uploadModel.UploadState = UploadState.Success;
+                    return uploadModel;
+                }
+            }
+
+            uploadModel.ErrorMessage = "Not Exist";
+            uploadModel.UploadState = UploadState.NotExist;
+
+            return uploadModel;
+        }
+    }
+}
---
+++ SEProject.MyBlogProject.WebApi/Models/UploadModel.cs
@@ -0,0 +1,11 @@
+﻿using SEProject.MyBlogProject.WebApi.Enums;
+
+namespace SEProject.MyBlogProject.WebApi.Models
+{
+    public class UploadModel
+    {
+        public string NewName { get; set; }
+        public string ErrorMessage { get; set; }
+        public UploadState UploadState { get; set; }
+    }
+}
